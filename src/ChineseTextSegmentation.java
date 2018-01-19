f309480c207872b3d3bb8d010da241654009eb64
wongkaho <wongkaho@158.132.4.54>
2018-01-19 17:08:07 +0800

    update ChineseTextSegmentation

--- src/ChineseTextSegmentation.java
+++ src/ChineseTextSegmentation.java
@@ -1,14 +1,9 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Scanner;
 
 import org.json.simple.JSONArray;
@@ -16,50 +11,44 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import com.huaban.analysis.jieba.WordDictionary;
 import com.chenlb.mmseg4j.ComplexSeg;
 import com.chenlb.mmseg4j.Dictionary;
 import com.chenlb.mmseg4j.MMSeg;
-import com.chenlb.mmseg4j.Seg;
 import com.chenlb.mmseg4j.Word;
-import com.huaban.analysis.jieba.JiebaSegmenter;
-import com.huaban.analysis.jieba.SegToken;
-import com.huaban.analysis.jieba.JiebaSegmenter.SegMode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.luhuiguo.chinese.ChineseUtils;
 
 public class ChineseTextSegmentation {
 	
 	protected Dictionary dic;  
 	
 	public ChineseTextSegmentation(){
-		System.setProperty("mmseg.dic.path", "data");  //這裡可以指定自訂詞庫  
-	    dic = Dictionary.getInstance();  
+		//set the dictionary path
+		System.setProperty("mmseg.dic.path", "data");   
+	    dic = Dictionary.getInstance();
 	}
 	
-	protected Seg getSeg() {  
-	    return new ComplexSeg(dic);  
-	}  
-	
-	public String segWords(String txt, String wordSpilt) throws IOException {  
-	    Reader input = new StringReader(txt);  
-	    StringBuilder sb = new StringBuilder();  
-	    Seg seg = getSeg();  
-	    MMSeg mmSeg = new MMSeg(input, seg);  
+	public String segmentation(String sentence, HashSet<String> stopwords) throws IOException {
+		Reader reader = new StringReader(sentence);  
+	    StringBuilder stringBuilder = new StringBuilder();  
+	    ComplexSeg seg = new ComplexSeg(dic);  
+	    MMSeg mmSeg = new MMSeg(reader, seg);  
 	    Word word = null;  
 	    boolean first = true;  
-	    while((word=mmSeg.next())!=null) {  
+	    while((word = mmSeg.next()) != null) {  
+	        if(stopwords.contains(word.getString())) {
+	        		break;
+	        }
 	        if(!first) {  
-	            sb.append(wordSpilt);  
-	        }  
-	        String w = word.getString();  
-	        sb.append(w);  
-	        first = false;        
+        			stringBuilder.append(" | ");  
+	        }
+	        stringBuilder.append(word.getString());
+	        first = false;
 	    }  
-	    return sb.toString();  
+	    return stringBuilder.toString();  
 	} 
 	
 	public static void main(String[] args) throws IOException, ParseException {		
-		
-		ChineseTextSegmentation chineseTextSegmentation = new ChineseTextSegmentation();
 		
 		//read Chinese stop words
 		Scanner stopwordScanner = new Scanner(new File("chinese_sw.txt"));
@@ -69,13 +58,22 @@ public class ChineseTextSegmentation {
 		}
 		stopwordScanner.close();
 
+		ChineseTextSegmentation chineseTextSegmentation = new ChineseTextSegmentation();
+
 		//start do the Chinese text segmentation
 		JSONParser parser = new JSONParser();
         JSONArray posts = (JSONArray) parser.parse(new FileReader("post.json"));
         for(Object object : posts) {
         		JSONObject post = (JSONObject) object;
-        		String title = (String) post.get("title");
-        		System.out.println(chineseTextSegmentation.segWords(title, " | "));
+        		String title = ChineseUtils.toTraditional((String) post.get("title"));
+        		String titleAfterSegmentation = chineseTextSegmentation.segmentation(title, stopwords);
+        	    System.out.println(titleAfterSegmentation);
+        	    	post.put("title", titleAfterSegmentation);
         }
+        
+        File file = new File("postAfterChineseTextSegmentation.json");
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.writeValue(file, posts);
 	}
+	
 }
