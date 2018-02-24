556be86ac02527e81ec309c09ca33102f565e3b0
wongkaho <wongkaho@192.168.1.5>
2018-02-24 19:48:18 +0800

    update CTS

--- src/ChineseTextSegmentation.java
+++ src/ChineseTextSegmentation.java
@@ -19,45 +19,41 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.luhuiguo.chinese.ChineseUtils;
 
 public class ChineseTextSegmentation {
-	
+
 	protected Dictionary dic;  
 	private static final String COMMENT = "/Users/wongkaho/Eclipse Workspace/ChineseTextSegmentation/resource/comment";
 	private static final String POST = "/Users/wongkaho/Eclipse Workspace/ChineseTextSegmentation/resource/post";
 
-	
+
 	public ChineseTextSegmentation(){
 		//set the dictionary path
 		System.setProperty("mmseg.dic.path", "data");   
-	    dic = Dictionary.getInstance();
+		dic = Dictionary.getInstance();
 	}
-	
+
 	public String segmentation(String sentence, HashSet<String> stopwords) throws IOException {
 		Reader reader = new StringReader(sentence);  
-	    StringBuilder stringBuilder = new StringBuilder();  
-	    ComplexSeg seg = new ComplexSeg(dic);  
-	    MMSeg mmSeg = new MMSeg(reader, seg);  
-	    Word word = null;
-	    boolean first = true;  
-	    while((word = mmSeg.next()) != null) {  
-//	        if(stopwords.contains(word.getString())) {
-//	        		break;
-//	        }
-	    		String w = word.getString();
-	        if(!first) {  
-		        	if (w.matches(".*[a-z].*")) { 
-		        		stringBuilder.append("  | "); 
-		        	}else {
-		        		stringBuilder.append(" | ");  
-		        	}
-	        }
-	        stringBuilder.append(w);
-	        first = false;
-	    }  
-	    return stringBuilder.toString();  
+		StringBuilder stringBuilder = new StringBuilder();  
+		ComplexSeg seg = new ComplexSeg(dic);  
+		MMSeg mmSeg = new MMSeg(reader, seg);  
+		Word word = null;
+		boolean first = true;  
+		while((word = mmSeg.next()) != null) {  
+			//	        if(stopwords.contains(word.getString())) {
+			//	        		break;
+			//	        }
+			String w = word.getString();
+			if(!first) {  
+				stringBuilder.append(" | ");  
+			}
+			stringBuilder.append(w);
+			first = false;
+		}  
+		return stringBuilder.toString();  
 	} 
-	
+
 	public static void main(String[] args) throws IOException, ParseException {		
-		
+
 		//read Chinese stop words
 		Scanner stopwordScanner = new Scanner(new File("chinese_sw.txt"));
 		HashSet<String> stopwords = new HashSet<String>();
@@ -70,46 +66,53 @@ public class ChineseTextSegmentation {
 
 		//start do the Chinese text segmentation on post
 		JSONParser parserPost = new JSONParser();
-        JSONArray posts = (JSONArray) parserPost.parse(new FileReader(POST + ".json"));
-        int cntPost = 0;
-        for(Object object : posts) {
-        		JSONObject post = (JSONObject) object;
-        		String title = ChineseUtils.toTraditional(filterWord((String) post.get("title").toString().trim()));
-        		String titleAfterSegmentation = chineseTextSegmentation.segmentation(title, stopwords);
-        		cntPost++;
-        		System.out.println(cntPost);
-        	    	post.put("title", titleAfterSegmentation);
-        }
-                
-        File filePost = new File("postAfterChineseTextSegmentation.json");
+		JSONArray posts = (JSONArray) parserPost.parse(new FileReader(POST + ".json"));
+		int cntPost = 0;
+		for(Object object : posts) {
+			JSONObject post = (JSONObject) object;
+			String title = ChineseUtils.toTraditional(filterWord((String) post.get("title").toString().trim()));
+			String titleAfterSegmentation = chineseTextSegmentation.segmentation(title, stopwords);
+			cntPost++;
+			System.out.println(cntPost);
+			post.put("title", titleAfterSegmentation);
+		}
+
+		File filePost = new File("postAfterChineseTextSegmentation.json");
 		ObjectMapper mapperPost = new ObjectMapper();
 		mapperPost.writeValue(filePost, posts);
-		
-	    //start do the Chinese text segmentation on comment
+
+		//start do the Chinese text segmentation on comment
 		for(int i = 1500; i < 2727; i++) {
-		JSONParser parserComment = new JSONParser();
-        JSONArray comments = (JSONArray) parserComment.parse(new FileReader(COMMENT + i + ".json"));
-        int cntComment = 0;
-        for(Object object : comments) {
-        		JSONObject comment = (JSONObject) object;
-        		JSONArray contents = (JSONArray) comment.get("content");
-        		JSONArray newContents = new JSONArray();
-        		for(Object content : contents) {
-        			String s = ChineseUtils.toTraditional(filterWord((String) content.toString().trim()));
-        			String sAfterSegmentation = chineseTextSegmentation.segmentation(s, stopwords);
-        			newContents.add(sAfterSegmentation);
-        			cntComment++;
-        			System.out.println(cntComment);
-        		}
-        		comment.put("content", newContents);
-        }
-        
-        File fileComment = new File(COMMENT + "AfterChineseTextSegmentation" + i + ".json");
-		ObjectMapper mapperComment = new ObjectMapper();
-		mapperComment.writeValue(fileComment, comments);
+			JSONParser parserComment = new JSONParser();
+			JSONArray comments = (JSONArray) parserComment.parse(new FileReader(COMMENT + i + ".json"));
+			int cntComment = 0;
+			for(Object object : comments) {
+				JSONObject comment = (JSONObject) object;
+				JSONArray contents = (JSONArray) comment.get("contents");
+				JSONArray newContents = new JSONArray();
+				for(Object o : contents) {
+					JSONObject jo = (JSONObject) o;
+					Object content = jo.get("content");
+					String I = jo.get("i").toString();
+					String s = ChineseUtils.toTraditional(filterWord((String) content.toString().trim()));
+					String sAfterSegmentation = chineseTextSegmentation.segmentation(s, stopwords);
+					JSONObject newjo = (JSONObject) o;
+					newjo.put("contentAfterSegmentation", sAfterSegmentation);
+					newjo.put("content", content.toString());
+					newjo.put("i", I);
+					newContents.add(newjo);
+					cntComment++;
+					System.out.println(cntComment);
+				}
+				comment.put("contents", newContents);
+			}
+
+			File fileComment = new File(COMMENT + "AfterChineseTextSegmentation" + i + ".json");
+			ObjectMapper mapperComment = new ObjectMapper();
+			mapperComment.writeValue(fileComment, comments);
 		}
 	}
-	
+
 	private static String filterWord(String input) {
 		String output = input;
 		if(input.contains(" [ 本帖最後由")) {
@@ -118,5 +121,5 @@ public class ChineseTextSegmentation {
 		}
 		return output;
 	}
-	
+
 }
